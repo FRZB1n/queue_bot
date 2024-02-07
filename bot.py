663744e8c75700b1e6b6cc4cec65dc1a0fae34d1
FERZ <arFReZa@yandex.ru>
2024-02-07 10:53:57 +0300

    Del done

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
-from core import admin_start, add_queue, admin_update
+from core import admin_start, add_queue, admin_update, delete_user
 from cfg import BOT_TOKEN
 
 
@@ -12,6 +12,12 @@ dp = Dispatcher(bot, storage=storage)
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     await admin_start(bot, message)
+
+@dp.message_handler(commands=['delete'])
+async def delete_user_h(message: types.Message):
+    await delete_user(bot, message)
+    # Проверяем, что после команды /delete есть аргумент (ID пользователя)
+    
 
 
 @dp.message_handler(commands=['update'])
--- core.py
+++ core.py
@@ -26,25 +26,48 @@ async def admin_update(bot:Bot, message:types.Message):
         
     await bot.send_message(message.chat.id, "Куда ты лезешь "+message.from_user.username+", щегол")
 
-            
+
+async def delete_user(bot:Bot, message:types.Message):
+    if message.from_user.username in ["FRZBin", "Fenix888YT", "Luks138"]:
+        try:
+            user_id = int(message.text.split()[1])  # Парсим ID пользователя из сообщения
+        except IndexError:
+            print("Вы не указали ID пользователя для удаления.")
+            return
+        except ValueError:
+            print("ID пользователя должен быть числом.")
+            return
+        
+        queue_list["list"].pop(user_id)
+        print('Успешно del')
+        new_list = {index: value for index, value in enumerate(queue_list["list"].values(), start=1)}
+        queue_list["list"] = new_list
+        await admin_update(bot, message)
+        return
+    
+    await bot.send_message(message.chat.id, "Куда ты лезешь "+message.from_user.username+", щегол")
 
 
-# 1030297121
+
 async def admin_start(bot:Bot, message:types.Message):
     if message.from_user.username in ["FRZBin", "Fenix888YT", "Luks138"]:
         queue_list['queue'].clear()
         queue_list['list'].clear()
         queue_list['queue'] = [798892590, 581496581]
-        queue_list['list'] = {1:"Fenix888YT", 2:"Luks138"}
+        queue_list['list'] = {1:"Fenix888YT(Ромзесссс None)", 2:"Luks138(Ярик None)"}
         prepare_array()
 
         queue_list['queue'].append(1030297121)
-        queue_list['list'][len(queue_list['queue'])] = "FRZBin"
+        queue_list['list'][len(queue_list['queue'])] = "FRZBin(None None)"
 
         if queue_list["message"]:
+            await bot.unpin_chat_message(message.chat.id, queue_list["message"].message_id)
             await bot.delete_message(message.chat.id, queue_list["message"].message_id)
         
         message = await bot.send_message(message.chat.id,"Занимаем очередь:", reply_markup=queue_start_kb())
+        
+
+        await bot.pin_chat_message(message.chat.id, message.message_id)
         queue_list["message"] = message
         return
     
@@ -56,11 +79,15 @@ async def admin_start(bot:Bot, message:types.Message):
 
 async def add_queue(bot:Bot, call:types.CallbackQuery):
     if call.from_user.id in queue_list['queue']:
-        await bot.answer_callback_query(call.id, "ДиНах", True)
+        await bot.answer_callback_query(call.id, "ДиНах")
         return
     
     queue_list['queue'].append(call.from_user.id)
-    queue_list['list'][len(queue_list['queue'])] = call.from_user.username
+
+    if call.from_user.username:
+        queue_list['list'][len(queue_list['queue'])] = str(call.from_user.username) + "(" + str(call.from_user.first_name) + " " + str(call.from_user.last_name) + ")"
+    else:
+        queue_list['list'][len(queue_list['queue'])] = str(call.from_user.first_name)
 
     text = ""
 
